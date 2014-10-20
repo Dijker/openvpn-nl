9f6afb22abae86b35bb34879ca209ff42e747a9a
Josh Datko <jbd@cryptotronix.com>
2014-10-20 03:31:15 +0000

    adds build.sh

---
+++ openvpn/build.sh
@@ -0,0 +1,4 @@
+./configure --disable-lzo --with-crypto-library=polarssl --enable-pkcs11 \
+POLARSSL_LIBS=-L/usr/local/lib
+#PKCS11_HELPER_LIBS=-lpkcs11-helper PKCS11_HELP_CFLAGS=-I/usr/local/include/pkcs11-helper-1.0/
+
